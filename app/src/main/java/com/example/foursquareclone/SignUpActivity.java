5bff7192ff41031e7c387b8f1d15ef4d7dbc897d
efsalokyay <35918590+efsalokyay@users.noreply.github.com>
2019-05-31 15:45:49 +0300

    Yazan: Efsal OKYAY

--- app/src/main/java/com/example/foursquareclone/SignUpActivity.java
+++ app/src/main/java/com/example/foursquareclone/SignUpActivity.java
@@ -16,7 +16,7 @@ import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText usernameText,passwordText;
+    EditText usernameText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +29,7 @@ public class SignUpActivity extends AppCompatActivity {
         ParseUser parseUser = ParseUser.getCurrentUser();
 
         if (parseUser != null) {
-            Intent intent = new Intent(getApplicationContext(),LocationsActivity.class);
+            Intent intent = new Intent(getApplicationContext(), LocationsActivity.class);
             startActivity(intent);
         }
     }
@@ -38,51 +38,53 @@ public class SignUpActivity extends AppCompatActivity {
 
         final ParseUser user = new ParseUser();
 
-        if (usernameText.getText().toString() != null && passwordText.getText().toString() != null){
+        if (!usernameText.getText().toString().matches("") && !passwordText.getText().toString().matches("")) {
 
             user.setUsername(usernameText.getText().toString());
-            user.setPassword(usernameText.getText().toString());
+            user.setPassword(passwordText.getText().toString());
 
             user.signUpInBackground(new SignUpCallback() {
                 @Override
                 public void done(ParseException e) {
 
-                    if (e != null){
+                    if (e != null) {
                         ParseUser.logOut();
-                        Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
-                    }
-                    else {
-                        alertDisplayer("Kayıt Başarılı!","Hoşgeldin: " + user.getUsername());
+                        alertDisplayer("Kayıt Başarısız", e.getLocalizedMessage() + "!!!");
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Kayıt Olundu.", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), LocationsActivity.class);
+                        startActivity(intent);
                     }
                 }
             });
-        }else
-            Toast.makeText(getApplicationContext(),"Lütfen alanları doldurunuz.",Toast.LENGTH_LONG).show();
+        } else
+            alertDisplayer("Kayıt Başarısız", "Gerekli alanları doldurunuz...");
     }
 
     public void signIn(View view) {
 
-        if (usernameText.getText().toString() != null && passwordText.getText().toString() != null) {
+        if (!usernameText.getText().toString().matches("") && !passwordText.getText().toString().matches("")) {
 
             System.out.println("Username: " + usernameText.getText());
 
             ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
                 @Override
                 public void done(ParseUser user, ParseException e) {
-                    if (user != null){
-                        alertDisplayer("Giriş Başarılı","Hoşgeldiniz: " + user.getUsername().toString() + "!");
-                    } else{
-                        System.out.println("Username: " +user.getUsername());
+                    if (user != null) {
+                        Toast.makeText(getApplicationContext(), "Hoşgeldiniz:  " + user.getUsername(), Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), LocationsActivity.class);
+                        startActivity(intent);
+                    } else {
                         ParseUser.logOut();
-                        Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                        alertDisplayer("Giriş Başarısız", e.getLocalizedMessage() + "!!!");
                     }
                 }
             });
-        }else
-            Toast.makeText(getApplicationContext(),"Lütfen alanları doldurunuz.",Toast.LENGTH_LONG).show();
+        } else
+            alertDisplayer("Giriş Başarısız", "Gerekli alanları doldurunuz...");
     }
 
-    private void alertDisplayer(String title,String message){
+    private void alertDisplayer(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
                 .setTitle(title)
                 .setMessage(message)
@@ -90,10 +92,6 @@ public class SignUpActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
-                        // don't forget to change the line below with the names of your Activities
-                        Intent intent = new Intent(SignUpActivity.this, LocationsActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
                     }
                 });
         AlertDialog ok = builder.create();
